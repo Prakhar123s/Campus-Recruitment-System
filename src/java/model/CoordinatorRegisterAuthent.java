1738771261c319a3ebe8b35e1404766920040fd9
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-06-19 08:47:44 +0530

    Add files via upload

---
+++ src/java/model/CoordinatorRegisterAuthent.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import db.MinorDatabase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class CoordinatorRegisterAuthent 
+{
+     public boolean coRegister(String username, String email, String phoneno, String password, String dob, String address, String gender)
+    {
+            try
+            {
+               
+                Connection con=MinorDatabase.getConnection();
+                PreparedStatement pst=con.prepareStatement("INSERT INTO coordinator values(?,?,?,?,?,?,?)");
+                
+                pst.setString(1, username);
+                pst.setString(2, email);
+                pst.setString(3, phoneno);
+                pst.setString(4, password);
+                pst.setString(5, dob);
+                pst.setString(6, address);
+                pst.setString(7, gender);
+                int i=pst.executeUpdate();  
+                System.out.println(i+" records inserted");
+            }
+            
+           catch(SQLException e)
+            {
+                System.out.println(e);
+            }
+            
+            if(username!=null && email!=null && phoneno!=null && password!=null && dob!=null)
+            {
+                System.out.println("true");
+                return true;
+            }
+            return false;
+        }     
+    
+}
