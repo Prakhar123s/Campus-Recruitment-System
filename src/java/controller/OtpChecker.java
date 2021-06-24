08f60af090feb5e36cf20a266f29a31ffaac2060
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-06-24 06:55:30 +0530

    Add files via upload

---
+++ src/java/controller/OtpChecker.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.mail.Session;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class OtpChecker extends HttpServlet 
+{
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+       String otp=request.getParameter("otp");
+       System.err.println("otp from html page:"+otp);
+       //String recivedotp=request.getParameter("otprecived");
+       //System.err.println("otp in hidden form:"+recivedotp);
+  
+        HttpSession session=request.getSession(true);  
+        String otpreciver=(String)session.getAttribute("MailOtp");
+        System.err.println("Hewllo the otp is :"+otpreciver);
+       
+       if(otp.equals(otpreciver))
+       {
+          response.sendRedirect("reset_password.html");
+       }
+       else
+       {
+           response.sendRedirect("otp_varification.jsp");
+       }
+
+        
+    }
+    
+  
+}
